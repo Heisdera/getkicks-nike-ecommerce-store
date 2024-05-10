b000a3736d0ed7a2538e7ff6694131bb8bcebc0a
Heisdera <heisdera@gmail.com>
2024-05-10 07:30:58 +0100

    update useProducts.ts

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import { useQuery } from "@tanstack/react-query";
-=======
-import { useState } from "react";
-import { useQuery, keepPreviousData } from "@tanstack/react-query";
-// import { useParams } from "react-router-dom";
->>>>>>> 68a3d8d09d1af83ffabd6ecfa97e8f068cbe10c4
 import { fetchProductsData } from "../services/fetchProductsData.ts";
 
 export function useProducts(page: number) {
