34a7b253f50db65871769886f5fedc4df1f0685d
Raphael Wisdom Chidera <heisdera@gmail.com>
2024-05-13 08:11:27 +0100

    Update useProducts.ts

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchProductsData } from "../services/fetchProductsData.ts";
 
-export function useProducts(collection: string, page: number) {
+export function useProducts(collection: string | undefined, page: number) {
   const { isLoading, data, error } = useQuery({
     queryKey: ["collection", collection, page],
     queryFn: () => fetchProductsData(collection, page),
