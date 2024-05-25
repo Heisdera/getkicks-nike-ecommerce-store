8d9bd6d7de2270cf893da1d3a5af8867ff20ee38
Raphael Wisdom Chidera <heisdera@gmail.com>
2024-05-25 07:35:08 +0100

    Bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import { ToastContainer, Slide } from "react-toastify";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 // import { Toaster } from "react-hot-toast";
 
 import router from "@/Routes/Routes";
@@ -41,5 +41,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Analytics />
       </QueryClientProvider>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
